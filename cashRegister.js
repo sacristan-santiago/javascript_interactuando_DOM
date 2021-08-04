d4392acaca3d49f716be1950e68863dc646b0c1f
Santiago Sacristan <sacristan.santiago@gmail.com>
2021-08-04 12:27:37 -0300

    se agrega funcionalidad para modificar dinero en caja

--- cashRegister.js
+++ cashRegister.js
@@ -1,7 +1,9 @@
 //cid IS THE AMOUNT OF CASH IN REGISTER BEFORE THE TRANSACTION.
 function checkCashRegister(price, cash, cid) {
-  price = Number(price);
-  cash = Number(cash);
+  console.log(cid)
+  console.log(price)
+  console.log(cash)
+
   let currencyUnit = ["PENNY", "NICKEL", "DIME", "QUARTER", "ONE", "FIVE", "TEN", "TWENTY", "ONE-HUNDRED"];
   let amount = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
   let output = {
@@ -11,7 +13,7 @@ function checkCashRegister(price, cash, cid) {
 
   let rest = cash - price;
   for (let i = amount.length-1; i>=0; i--) {
-    let arr = [currencyUnit[i], 0];
+    let arr = [currencyUnit[i], 0]; 
     while (amount[i]<=rest && cid[i][1]>=arr[1]+amount[i]) {
       rest = Math.round((rest-amount[i])*100)/100;
       arr[1] += amount[i];
@@ -21,16 +23,19 @@ function checkCashRegister(price, cash, cid) {
   }
   // console.log(output.change)
   
+  //COMPARATIVE FUNCTIONS
+  const availableChange = (output) => Math.round((output.change.reduce((acc,curr)=> acc += curr[1], 0)*100))/100;
+  
+  const neededChange = (cash, price) => Math.round((cash-price)*100)/100;
+  console.log(neededChange(cash, price))
+  const moneyInRegister = (cid) => Math.round((cid.reduce((acc,curr)=> acc += curr[1], 0)*100))/100
+  console.log(moneyInRegister(cid))
   //CONDITIONAL TO CHECK IF CASHREGISTER REMAINS OPEN, CLOSED OR HAS NO FUNDS.
-  if (Math.round((output.change.reduce((acc,curr)=> acc += curr[1], 0)*100))/100 < Math.round((cash-price)*100)/100) {
+  if (availableChange(output) < neededChange(cash, price)) {
       output.status = "INSUFFICIENT_FUNDS";
-      // console.log(cash);
-      // console.log(price)
-      // console.log(Math.round((cash-price)*100)/100);
-      // console.log(Math.round((output.change.reduce((acc,curr)=> acc += curr[1], 0)*100))/100);
       output.change = [];
     }
-    else if (Math.round((cid.reduce((acc,curr)=> acc += curr[1], 0)*100))/100 === cash-price) {
+    else if (moneyInRegister(cid) === neededChange(cash, price)) {
       output.status = "CLOSED";
       output.change = output.change.filter(x=>x[1] != 0)
     } 
@@ -38,7 +43,5 @@ function checkCashRegister(price, cash, cid) {
       output.status = "OPEN";
       output.change = output.change.filter(x=>x[1] != 0)
     }
-  
-  // console.log(output)
   return output;
 }
